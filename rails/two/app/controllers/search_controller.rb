7dfdb7f946377f321b79c3f7d99c4032fafff8e1
Dimitar Bonev <dimitar.bonev@gmail.com>
2014-12-01 00:13:33 +0200

    some comments

--- rails/two/app/controllers/search_controller.rb
+++ rails/two/app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def get
   	@search_term = params[:q]
+	# we should use parametrized query here
 	@projects = Project.where("title like \"%#{@search_term}%\"")
   end
 end
