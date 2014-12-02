207059f5266d6a6451d298fb5a6d86d58c9fbf48
Dimitar Bonev <dimitar.bonev@gmail.com>
2014-12-02 22:34:30 +0200

    to merge

--- app/models/subcontractor.rb
+++ app/models/subcontractor.rb
@@ -1,3 +1,12 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+	unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	  record.errors[attribute] << (options[:message] || "is not an email")
+	end
+  end
+end
+
 class Subcontractor < ActiveRecord::Base
 	validates :name,:email, :description, :presence => true
+	validates :email, :email => true
 end
