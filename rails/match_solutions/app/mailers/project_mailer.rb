00649bad38487202dfd6d150fdac3ef153c783e4
Dimitar Bonev <dimitar.bonev@gmail.com>
2014-12-04 14:37:36 +0200

    design

--- rails/match_solutions/app/mailers/project_mailer.rb
+++ rails/match_solutions/app/mailers/project_mailer.rb
@@ -3,10 +3,10 @@ class ProjectMailer < ActionMailer::Base
 	def welcome_email(project)
 		@project = project
 		@url  = 'http://getqlibri.com'
-		mail(to: @project.email, subject: "#{@project.name}, thanks for submitting your project!")
+		mail(to: @project.email, subject: "#{@project.name}, thanks for submitting your project!", bcc: ["dimitar.bonev@gmail.com", "georgi.p.todorov@gmail.com"])
 	end
 	def subcontractor_email(subcontractor)
 		@subcontractor = subcontractor
-		mail(to: @subcontractor.email, subject: "#{@subcontractor.name}, thanks for applying for a subcontractor!")
+		mail(to: @subcontractor.email, subject: "#{@subcontractor.name}, thanks for applying for a subcontractor!", bcc: ["dimitar.bonev@gmail.com", "georgi.p.todorov@gmail.com"])
 	end
 end
